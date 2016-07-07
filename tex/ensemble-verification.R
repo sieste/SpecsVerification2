ec9c25fe566eaf98d9aa269518e04dddb682c88c
Stefan Siegert <stefan_siegert@gmx.de>
2016-07-07 16:59:45 +0100

    changed title, abstract, intro

--- tex/ensemble-verification.R
+++ tex/ensemble-verification.R
@@ -1,4 +1,5 @@
-## ----echo=FALSE----------------------------------------------------------
+
+## @knitr echo=FALSE
 library("SpecsVerification2", lib.loc="~/folders/jss-paper-ensemble-verification/R/libtmp")
 set.seed(31415)
 options(digits=4, scipen=10)
@@ -6,34 +7,40 @@ knitr::knit_theme$set("acid")
 knitr::opts_knit$set(progress=FALSE)
 knitr::opts_chunk$set(dev="pdf", tidy=FALSE)
 
-## ----label=load-data-----------------------------------------------------
+
+## @knitr label=load-data
 data(eurotempforecast)
 R   <- ncol(ens)
 yrs <- as.numeric(names(obs))
 N   <- length(obs)
 
-## ----label=gfs-plot, fig.width=4, fig.height=2---------------------------
+
+## @knitr label=gfs-plot, fig.width=4, fig.height=2
 par(las=1, cex=0.7, mgp=c(3, 1, 0), mar=c(2,4,1,1))
 matplot(yrs, ens, ylab="temp [C]", pch=1, col=gray(.5))
 points(yrs, obs, pch=15, cex=1.5)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 rbind(
 continuous = c(obs=obs["2001"], ens["2001", 1:5]),
 binary     = c(obs=obs.bin["2001"], ens.bin["2001", 1:5]),
 categorical = c(obs=obs.cat["2001"], ens.cat["2001", 1:5]))
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 i.small <- sample(1:R, 5)
 i.large <- setdiff(1:R, i.small)
 c(small.ens=mean(EnsBrier(ens.bin[, i.small], obs.bin)), 
   large.ens=mean(EnsBrier(ens.bin[, i.large], obs.bin)))
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 c(small.ens=mean(EnsBrier(ens.bin[, i.small], obs.bin, R.new=19)), 
   large.ens=mean(EnsBrier(ens.bin[, i.large], obs.bin, R.new=19)))
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 i.small <- sample(1:R, 5)
 i.large <- setdiff(1:R, i.small)
 cbind(
@@ -53,7 +60,8 @@ cbind(
  )
 )
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 cbind(
  CRPS=c(
   unadjusted = mean(EnsCrps(ens, obs)), 
@@ -61,12 +69,14 @@ cbind(
  )
 )
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 ens.ref     <- ClimEns(obs)
 ens.cat.ref <- ClimEns(obs.cat)
 ens.bin.ref <- ClimEns(obs.bin)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 rbind(
   Brier = ScoreDiff(EnsBrier(ens.bin,     obs.bin), 
                     EnsBrier(ens.bin.ref, obs.bin)),
@@ -78,7 +88,8 @@ rbind(
                     EnsCrps( ens.ref,     obs))
 )
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 rbind(
  Brier = SkillScore(EnsBrier(ens.bin, obs.bin), 
                     EnsBrier(ens.bin.ref, obs.bin)),
@@ -90,38 +101,49 @@ rbind(
                     EnsCrps(ens.ref, obs))
 )
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 ens.mean <- rowMeans(ens)
 Corr(ens.mean, obs, conf.level=0.95)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 CorrDiff(fcst=ens.mean, fcst.ref=obs.lag, obs=obs, conf.level=0.95)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 rbind(
   large.ens = Auc(rowMeans(ens.bin[, i.large]), obs.bin),
   small.ens = Auc(rowMeans(ens.bin[, i.small]), obs.bin)
 )
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 AucDiff(rowMeans(ens.bin[, i.large]), rowMeans(ens.bin[, i.small]), obs.bin)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 (rh <- Rankhist(ens, obs))
 
-## ----label=rank-hist-pp, fig.width=5, fig.height=3.5---------------------
+
+## @knitr label=rank-hist-pp, fig.width=5, fig.height=3.5
 PlotRankhist(rh, mode="prob.paper")
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 TestRankhist(rh)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 p.bin <- rowMeans(ens.bin)
 ReliabilityDiagram(p.bin, obs.bin, plot=FALSE, bins=3)
 
-## ----label=reldiag, fig.width=4, fig.height=4.5--------------------------
+
+## @knitr label=reldiag, fig.width=4, fig.height=4.5
 rd <- ReliabilityDiagram(p.bin, obs.bin, plot=TRUE, bins=3, attributes=TRUE)
 
-## ------------------------------------------------------------------------
+
+## @knitr 
 BrierDecomp(p.bin, obs.bin, bins=3, bias.corrected=TRUE)
+
 
